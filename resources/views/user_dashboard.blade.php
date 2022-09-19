e3ef840c94aa278429a560c94d6f8cc13f413bbc
tena <tena.visansoft@gmail.com>
2022-09-19 15:12:23 +0530

    samall change in amount

--- resources/views/user_dashboard.blade.php
+++ resources/views/user_dashboard.blade.php
@@ -1784,7 +1784,7 @@
                     }
                 }
 
-                var amnt = 100000; 
+                var amnt = amount; 
                 //amount-loop
                 for (var i = 1; i <= 4; i++){
 
