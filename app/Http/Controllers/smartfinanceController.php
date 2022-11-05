075b350d0c7c948504cc3dd1f32b8435c15fbfc0
tena <tena.visansoft@gmail.com>
2022-11-05 14:25:01 +0530

    testing email

--- app/Http/Controllers/smartfinanceController.php
+++ app/Http/Controllers/smartfinanceController.php
@@ -1126,7 +1126,8 @@ class smartfinanceController extends Controller
         $time = Carbon::now()->toTimeString();
         $date = Carbon::now()->formatLocalized('%d %b %Y');
         $now = $date.'_'.$time;
-        $name= $date.'.xlsx';
+        $da = Carbon::now()->toDateString();
+        $name= $da.'.xlsx';
         Excel::store(new NextMonthPayoutsExport(2018), $name,'excel');
 
 
