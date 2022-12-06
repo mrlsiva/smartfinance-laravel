d53dbb599393b0ed0d82d2c51d6b23f361cdaaec
tena <tena.visansoft@gmail.com>
2022-12-06 12:54:37 +0530

    mail bug

--- app/Http/Controllers/taxController.php
+++ app/Http/Controllers/taxController.php
@@ -55,7 +55,7 @@ class taxController extends Controller
 
             $filename = $file->getClientOriginalName(); 
             $filename = time().$filename;
-            $path = $file->storeAs('/public/'.config('path.tax_document'), $filename);
+            $path = $file->storeAs('/public/tax_document/',$filename);
             $filenames .= $filename.",";
 
         }
