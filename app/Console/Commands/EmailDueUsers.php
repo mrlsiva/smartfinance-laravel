2f24b186a9fc320ab1eaf6283d20992887607fc3
tena <tena.visansoft@gmail.com>
2022-12-16 19:39:20 +0530

    cron

--- app/Console/Commands/EmailDueUsers.php
+++ app/Console/Commands/EmailDueUsers.php
@@ -44,46 +44,48 @@ class EmailDueUsers extends Command
      */
     public function handle()
     {
-        $now = Carbon::now()->format('Y-m-d');
-        $notifications = InsuranceNotification::where([['date',$now],['is_send',0]])->get();
-        foreach ($notifications as $notification) {
-            $emailsetting = Template::where([['id',39],['is_active',1]])->first(); 
-            if($emailsetting != null){
-                $email_template = $emailsetting->template;
-                $emailContentReplace=['##NAME##'=>$notification->insurance->user->first_name.' '.$notification->insurance->user->last_name];
-                $txt = strtr($email_template,$emailContentReplace);
-                $emailId = $notification->insurance->user->email;
-                $subject = $emailsetting->subject;
-                $mailstatus = SMTPController::sendMail($emailId,$subject,$txt);
-            }
-            $insurance_notification = DB::table('insurance_notifications')->where('id',$notification->id)->update(['is_send' =>1]);
+        $insurance_notification = DB::table('insurance_notifications')->where('id',7)->update(['is_send' =>1]);
 
-            if($notification->insurance->due == 'Monthly'){
+        // $now = Carbon::now()->format('Y-m-d');
+        // $notifications = InsuranceNotification::where([['date',$now],['is_send',0]])->get();
+        // foreach ($notifications as $notification) {
+        //     $emailsetting = Template::where([['id',39],['is_active',1]])->first(); 
+        //     if($emailsetting != null){
+        //         $email_template = $emailsetting->template;
+        //         $emailContentReplace=['##NAME##'=>$notification->insurance->user->first_name.' '.$notification->insurance->user->last_name];
+        //         $txt = strtr($email_template,$emailContentReplace);
+        //         $emailId = $notification->insurance->user->email;
+        //         $subject = $emailsetting->subject;
+        //         $mailstatus = SMTPController::sendMail($emailId,$subject,$txt);
+        //     }
+        //     $insurance_notification = DB::table('insurance_notifications')->where('id',$notification->id)->update(['is_send' =>1]);
 
-                $date = Carbon::parse($notification->date)->addMonths(1);
-            }
+        //     if($notification->insurance->due == 'Monthly'){
 
-            if($notification->insurance->due == 'Yearly'){
+        //         $date = Carbon::parse($notification->date)->addMonths(1);
+        //     }
 
-                $date = Carbon::parse($notification->date)->addYears(1);
-            }
+        //     if($notification->insurance->due == 'Yearly'){
 
-            if($notification->insurance->due == 'Quarterly'){
+        //         $date = Carbon::parse($notification->date)->addYears(1);
+        //     }
 
-                $date = Carbon::parse($notification->date)->addMonths(3);
-            }
+        //     if($notification->insurance->due == 'Quarterly'){
+
+        //         $date = Carbon::parse($notification->date)->addMonths(3);
+        //     }
 
 
-            if($date < $notification->insurance->end_date){
+        //     if($date < $notification->insurance->end_date){
 
-                $insurance_notification = InsuranceNotification::create([
-                    'insurance_id' => $notification->insurance_id,
-                    'date' => $date,
-                    'is_send' => 0,
-                ]);
-            } 
-        }
+        //         $insurance_notification = InsuranceNotification::create([
+        //             'insurance_id' => $notification->insurance_id,
+        //             'date' => $date,
+        //             'is_send' => 0,
+        //         ]);
+        //     } 
+        // }
 
-        echo $notifications->count()." mails was send in ".$now."\n";
+        //echo $notifications->count()." mails was send in ".$now."\n";
     }
 }
