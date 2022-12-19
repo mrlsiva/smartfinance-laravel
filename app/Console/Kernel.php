4947752b465a2b83312479467b366f4bf586693f
tena <tena.visansoft@gmail.com>
2022-12-19 11:49:00 +0530

    cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,8 +3,6 @@
 namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Console\Commands\EmailDueUsers;
-use App\Console\Commands\PayoutCron;
 use App\Console\Commands\WinnerCron;
 use App\Models\User;
 
@@ -19,18 +17,12 @@ class Kernel extends ConsoleKernel
 
     protected $commands = [
         \App\Console\Commands\WinnerCron::class,
-        \App\Console\Commands\EmailDueUsers::class,
-        \App\Console\Commands\PayoutCron::class,
     ];
 
     protected function schedule(Schedule $schedule)
     {
 
         $schedule->command('winner:cron')->everyMinute();
-        $filePath = 'public/insurance_cron.txt';
-        $schedule->command('email:users')->everyMinute()->appendOutputTo($filePath);
-        $filePath = 'public/payout_cron.txt';
-        $schedule->command('payout:cron')->everyMinute()->appendOutputTo($filePath);
     }
 
     /**
