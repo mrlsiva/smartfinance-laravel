b563f43e22d469b3e341d880b1251fc7c6a3b50e
tena <tena.visansoft@gmail.com>
2022-12-19 21:42:01 +0530

    cron payout controller bug

--- app/Http/Controllers/cronController.php
+++ app/Http/Controllers/cronController.php
@@ -70,97 +70,104 @@ class cronController extends Controller
 
     public function payoutList(Request $request)
     {
-        $month = Carbon::now()->addMonth()->format('m');
-        $year = Carbon::now()->addMonth()->format('Y');
+        $month_end = Carbon::now()->endOfMonth()->format('Y-m-d');
+        $now_time = Carbon::now()->format('H');
+        $now = Carbon::now()->format('Y-m-d');
 
-        $payout_delete = NextMonthPayout::truncate();
+        if($now == $month_end && $now_time == '21'){
 
-        $users = SmartfinancePayment::join('smartfinances','smartfinance_payments.smartfinance_id','=','smartfinances.id')->whereMonth('smartfinance_payments.payment_date',$month)->whereYear('smartfinance_payments.payment_date', $year)->groupBy('smartfinances.user_id')->select('smartfinances.user_id')->get();
+            $month = Carbon::now()->addMonth()->format('m');
+            $year = Carbon::now()->addMonth()->format('Y');
 
-        foreach($users as $user){
+            $payout_delete = NextMonthPayout::truncate();
 
-            $payments = SmartfinancePayment::join('smartfinances','smartfinance_payments.smartfinance_id','=','smartfinances.id')->whereMonth('smartfinance_payments.payment_date',$month)->whereYear('smartfinance_payments.payment_date', $year)->where('smartfinances.user_id',$user->user_id)->where('smartfinances.plan_id','!=',3)->select('smartfinance_payments.*','smartfinances.user_id','smartfinances.plan_id')->get();
-            $amount = 0;
-            foreach($payments as $payment){
+            $users = SmartfinancePayment::join('smartfinances','smartfinance_payments.smartfinance_id','=','smartfinances.id')->whereMonth('smartfinance_payments.payment_date',$month)->whereYear('smartfinance_payments.payment_date', $year)->groupBy('smartfinances.user_id')->select('smartfinances.user_id')->get();
 
-                $amount = $amount + $payment->amount;
-            }
-            $payments = SmartfinancePayment::join('smartfinances','smartfinance_payments.smartfinance_id','=','smartfinances.id')->whereMonth('smartfinance_payments.payment_date',$month)->whereYear('smartfinance_payments.payment_date', $year)->where('smartfinances.user_id',$user->user_id)->where('smartfinances.plan_id','=',3)->orderBy('smartfinance_payments.id','Desc')->groupBy('smartfinance_payments.smartfinance_id')->select('smartfinance_payments.*','smartfinances.user_id','smartfinances.plan_id')->get();
-            foreach($payments as $payment){
+            foreach($users as $user){
 
-                $payment_ym = SmartfinancePayment::where('smartfinance_id',$payment->smartfinance_id)->orderBy('id','Desc')->first();
-                $amount = $amount + $payment_ym->next_amount + $payment_ym->intrest + $payment_ym->balance;
-            }
+                $payments = SmartfinancePayment::join('smartfinances','smartfinance_payments.smartfinance_id','=','smartfinances.id')->whereMonth('smartfinance_payments.payment_date',$month)->whereYear('smartfinance_payments.payment_date', $year)->where('smartfinances.user_id',$user->user_id)->where('smartfinances.plan_id','!=',3)->select('smartfinance_payments.*','smartfinances.user_id','smartfinances.plan_id')->get();
+                $amount = 0;
+                foreach($payments as $payment){
 
-            $result=[];
-            $smartfinance_ids = Smartfinance::where('user_id',$user->user_id)->get();
-            foreach($smartfinance_ids as $smartfinance_id){
-                $result[] = $smartfinance_id->id;
-            }
-            $next_payment_date = SmartfinancePayment::whereIn('smartfinance_id',$result)->where('is_status',0)->orderBy('payment_date', 'asc')->first();
-            $user_amount = UserAmount::where([['user_id',$user->user_id],['is_status',0]])->first();
-
-            if($next_payment_date->payment_date == $payment->payment_date )
-            {
-                if($user_amount != NULL){
-
-                    $amount = $amount +  $user_amount->amount;
+                    $amount = $amount + $payment->amount;
                 }
+                $payments = SmartfinancePayment::join('smartfinances','smartfinance_payments.smartfinance_id','=','smartfinances.id')->whereMonth('smartfinance_payments.payment_date',$month)->whereYear('smartfinance_payments.payment_date', $year)->where('smartfinances.user_id',$user->user_id)->where('smartfinances.plan_id','=',3)->orderBy('smartfinance_payments.id','Desc')->groupBy('smartfinance_payments.smartfinance_id')->select('smartfinance_payments.*','smartfinances.user_id','smartfinances.plan_id')->get();
+                foreach($payments as $payment){
+
+                    $payment_ym = SmartfinancePayment::where('smartfinance_id',$payment->smartfinance_id)->orderBy('id','Desc')->first();
+                    $amount = $amount + $payment_ym->next_amount + $payment_ym->intrest + $payment_ym->balance;
+                }
+
+                $result=[];
+                $smartfinance_ids = Smartfinance::where('user_id',$user->user_id)->get();
+                foreach($smartfinance_ids as $smartfinance_id){
+                    $result[] = $smartfinance_id->id;
+                }
+                $next_payment_date = SmartfinancePayment::whereIn('smartfinance_id',$result)->where('is_status',0)->orderBy('payment_date', 'asc')->first();
+                $user_amount = UserAmount::where([['user_id',$user->user_id],['is_status',0]])->first();
+
+                if($next_payment_date->payment_date == $payment->payment_date )
+                {
+                    if($user_amount != NULL){
+
+                        $amount = $amount +  $user_amount->amount;
+                    }
+                }
+                //return $amount;
+
+
+                $payout = NextMonthPayout::create([
+
+                    'user_id' => $payment->smartfinance->user->id,
+                    'name' => $payment->smartfinance->user->first_name.' '.$payment->smartfinance->user->last_name,
+                    'date' => $payment->payment_date,
+                    'next_payout_amount' => $amount
+                ]);
             }
-            //return $amount;
+
+            $user_amounts = UserAmount::whereNotIn('user_id',$users)->where('is_status',0)->get();
+            foreach($user_amounts as $user_amount){
+                $date = Carbon::parse($user_amount->date)->addMonths(1);
+                $new_date = Carbon::parse($date)->setDay(6)->format('Y-m-d');
+
+                $timestamp = strtotime($new_date);
+                $day = date('l', $timestamp);
+                if($day == 'Tuesday' ||$day == 'Sunday' ||$day == 'Friday'){
+                    $date = Carbon::parse($new_date)->setDay(7)->format('Y-m-d');
+                }
+
+                $payout = NextMonthPayout::create([
+
+                    'user_id' => $user_amount->user->id,
+                    'name' =>  $user_amount->user->first_name.' '. $user_amount->user->last_name,
+                    'date' => $date,
+                    'next_payout_amount' => $user_amount->amount
+                ]);
+
+            } 
+
+            $time = Carbon::now()->toTimeString();
+            $date = Carbon::now()->formatLocalized('%d %b %Y');
+            $now = $date.'_'.$time;
+            $name= $date.'.xlsx';
+            Storage::disk('public')->delete('export/'.$name);
+            Excel::store(new NextMonthPayoutsExport(2018), $name,'excel');
 
 
-            $payout = NextMonthPayout::create([
-
-                'user_id' => $payment->smartfinance->user->id,
-                'name' => $payment->smartfinance->user->first_name.' '.$payment->smartfinance->user->last_name,
-                'date' => $payment->payment_date,
-                'next_payout_amount' => $amount
-            ]);
-        }
-
-        $user_amounts = UserAmount::whereNotIn('user_id',$users)->where('is_status',0)->get();
-        foreach($user_amounts as $user_amount){
-            $date = Carbon::parse($user_amount->date)->addMonths(1);
-            $new_date = Carbon::parse($date)->setDay(6)->format('Y-m-d');
-
-            $timestamp = strtotime($new_date);
-            $day = date('l', $timestamp);
-            if($day == 'Tuesday' ||$day == 'Sunday' ||$day == 'Friday'){
-                $date = Carbon::parse($new_date)->setDay(7)->format('Y-m-d');
+            //Mail
+            $attachment = Storage::path('/public/excel/'.$name);
+            $emailsetting = Template::where([['id',20],['is_active',1]])->first(); 
+            $admin_email = Setting::where('key','admin_email')->first();
+            if($emailsetting != null){
+                $txt = $emailsetting->template;
+                $emailId = $admin_email->value;
+                $subject = $emailsetting->subject;
+                $mailstatus = SMTPController::sendMail($emailId,$subject,$txt,$attachment);
             }
-
-            $payout = NextMonthPayout::create([
-
-                'user_id' => $user_amount->user->id,
-                'name' =>  $user_amount->user->first_name.' '. $user_amount->user->last_name,
-                'date' => $date,
-                'next_payout_amount' => $user_amount->amount
-            ]);
-
-        } 
-
-        $time = Carbon::now()->toTimeString();
-        $date = Carbon::now()->formatLocalized('%d %b %Y');
-        $now = $date.'_'.$time;
-        $name= $date.'.xlsx';
-        Storage::disk('public')->delete('export/'.$name);
-        Excel::store(new NextMonthPayoutsExport(2018), $name,'excel');
-
-
-        //Mail
-        $attachment = Storage::path('/public/excel/'.$name);
-        $emailsetting = Template::where([['id',20],['is_active',1]])->first(); 
-        $admin_email = Setting::where('key','admin_email')->first();
-        if($emailsetting != null){
-            $txt = $emailsetting->template;
-            $emailId = $admin_email->value;
-            $subject = $emailsetting->subject;
-            $mailstatus = SMTPController::sendMail($emailId,$subject,$txt,$attachment);
+            //Mail End 
         }
-        //Mail End 
+        else{
 
+        }  
     }
-
-
 }
